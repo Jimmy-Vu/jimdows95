1d29f1b55ad874d8e64b3c8b479ce5f7d60791fc
Jimmy Vu <JMV3211@gmail.com>
2023-08-23 18:10:54 -0700

    Add the menu button and time container for taskbar

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react"
+
 export default function Taskbar() {
+
+  const [currTime, setCurrTime] = useState(new Date().toLocaleTimeString(undefined, {
+    hour12: true, // Keep AM/PM indicator
+    hour: 'numeric',
+    minute: 'numeric'
+  }));
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrTime(new Date().toLocaleTimeString(undefined, {
+        hour12: true, // Keep AM/PM indicator
+        hour: 'numeric',
+        minute: 'numeric'
+      }))}, 1000)
+  }, [])
 
   return (
     <>
       <section className="taskbar">
-
+        <button className="taskbar__menu-btn">Start</button>
+        <div className="taskbar__time">
+            <p>{currTime}</p>
+        </div>
       </section>
     </>
   )

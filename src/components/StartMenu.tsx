ea020ec2456db3147bab96140f9cf0508f8cdd4d
Jimmy Vu <JMV3211@gmail.com>
2023-08-25 17:54:14 -0700

    Complete the app drawer of the start menu

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -9,21 +9,25 @@ export default function StartMenu(props: { menuIsActive: boolean }) {
       <section className="start-menu__app-drawer">
         <div className="app-drawer__programs">
           <a href="https://www.linkedin.com/in/jimmyvu2/" target="_blank" className="programs__item" id="linkedin">
-            <img src="/src/assets/linkedin-icon.png" alt="icon of an address book" />
-            <p className="programs__item__text">My LinkedIn</p>
+            <img className="programs__item__icon" src="/src/assets/linkedin-icon.png" alt="icon of an address book" />
+            <p className="programs__item__text">LinkedIn</p>
           </a>
           <a href="https://github.com/Jimmy-Vu" target="_blank" className="programs__item" id="github">
-            <img src="/src/assets/github-icon.png" alt="icon of two computers connecting to a cloud" />
+            <img className="programs__item__icon" src="/src/assets/github-icon.png" alt="icon of two computers connecting to a cloud" />
             <p className="programs__item__text">GitHub</p>
           </a>
-          <a href="https://dimden.dev/" className="programs__item" id="dimdev">
-            <img src="/src/assets/dimdev-icon.png" alt="icon of internet explorer" />
-            <p className="programs__item__text">A Pretty Cool Retro Site</p>
+          <a href="https://twitter.com/thrownewJimmy" target="_blank" className="programs__item" id="twitter">
+            <img className="programs__item__icon" src="/src/assets/twitter-icon.png" alt="icon of two computers connecting to a cloud" />
+            <p className="programs__item__text">Twitter</p>
+          </a>
+          <a href="https://dimden.dev/" target="_blank" className="programs__item" id="dimdev">
+            <img className="programs__item__icon" src="/src/assets/dimdev-icon.png" alt="icon of internet explorer" />
+            <p className="programs__item__text">Cool Retro Site</p>
           </a>
         </div>
         <button className="app-drawer__shut-down">
-          <img src="/src/assets/shut_down_with_computer-0.png" alt="icon for the shut down button" />
-          <p>Shut Down</p>
+          <img className="shut-down__icon" src="/src/assets/shut_down_with_computer-0.png" alt="icon for the shut down button" />
+          <p className="shut-down__text">Shut Down</p>
         </button>
       </section>
     </div>

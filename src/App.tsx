3a94ecb742425b852ebd9cd8ce5b08bb8d84cbba
Jimmy Vu <JMV3211@gmail.com>
2023-08-28 20:20:09 -0700

    Place skeleton of the desktop icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Taskbar from "./components/Taskbar"
+import DesktopIcons from "./components/DesktopIcons"
+
 function App() {
 
   return (
     <>
       <main>
+        <DesktopIcons />
         <Taskbar />
       </main>
     </>

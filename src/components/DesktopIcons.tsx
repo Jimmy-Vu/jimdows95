07b83761d025defa0450c4b8caedface2d7ad51f
Jimmy Vu <JMV3211@gmail.com>
2023-08-29 20:00:55 -0700

    Adjust icon size and add function for handling clicks

--- src/components/DesktopIcons.tsx
+++ src/components/DesktopIcons.tsx
@@ -1,22 +1,28 @@
 export default function DesktopIcons() {
+  function handleClick(event: React.MouseEvent<HTMLElement>) {
+    if (event.target && event.target instanceof HTMLElement) {
+      console.log(event.target.closest('.icon')?.getAttribute('data-item'));
+    }
+  }
+
   return (
     <section className="desktop-icons">
-      <div className="icon about-me">
-        <button id="about-me__icon-img" className="icon-img about-me__icon-img">
+      <div data-item="about-me" className="icon about-me">
+        <button id="about-me__icon-img" className="icon-img about-me__icon-img" onClick={handleClick}>
           <img src="src/assets/about-me-icon.png" alt="an icon of a man's head" />
         </button>
         <label htmlFor="about-me__icon-img" className="about-me__label">About Me</label>
       </div>
 
-      <div className="icon projects">
-        <button id="projects__icon-img" className="icon-img projects__icon-img">
+      <div data-item="projects" className="icon projects">
+        <button id="projects__icon-img" className="icon-img projects__icon-img" onClick={handleClick}>
           <img src="src/assets/open-folder-icon.png" alt="an icon of an opened folder with a sheet of paper popping out" />
         </button>
         <label htmlFor="projects__icon-img" className="projects__label">Projects</label>
       </div>
 
-      <div className="icon contact">
-        <button id="contact__icon-img" className="icon-img contact__icon-img">
+      <div data-item="contact" className="icon contact">
+        <button id="contact__icon-img" className="icon-img contact__icon-img" onClick={handleClick}>
           <img src="src/assets/aim-icon.png" alt="an icon of a yellow illusrated person in a walking position" />
         </button>
         <label htmlFor="contact__icon-img" className="contact__label">Contact Me</label>

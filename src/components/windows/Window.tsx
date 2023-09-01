c62182003a28473792bcfea32d733eda43c1fedb
Jimmy Vu <JMV3211@gmail.com>
2023-08-31 22:03:33 -0700

    Fix the windows issue with expanding in both directions on resizing pull

--- src/components/windows/Window.tsx
+++ src/components/windows/Window.tsx
@@ -3,23 +3,19 @@ import { Resizable } from 're-resizable';
 
 export default function Window() {
   return (
-    <Draggable
-      handle=".window__header"
-      // bounds="parent"
-      defaultPosition={{ x: 0, y: 0 }}
-      positionOffset={{ x: '-50%', y: '-50%' }}
-    >
+    <Draggable handle=".window__header">
       <Resizable
         className="window"
         handleComponent={{ bottomRight: <img style={{ transform: 'translateX(-10px) translateY(-10px)' }} src="src/assets/resize-right.svg" alt="" /> }}
-        handleClasses={{
-          top: "pointer-events-none",
-          right: "pointer-events-none",
-          bottom: "pointer-events-none",
-          left: "pointer-events-none",
-          topRight: "pointer-events-none",
-          bottomLeft: "pointer-events-none",
-          topLeft: "pointer-events-none",
+        enable={{
+          top: false,
+          right: false,
+          bottom: false,
+          left: false,
+          topRight: false,
+          bottomRight: true,
+          bottomLeft: false,
+          topLeft: false
         }}
         defaultSize={{
           width: 600,
@@ -27,8 +23,7 @@ export default function Window() {
         }}
         minWidth={600}
         minHeight={400}
-        bounds="parent"
-        style={{ position: 'absolute', left: '50%', top: '50%' }}
+        style={{ position: 'absolute', left: '30%', top: '20%' }}
       >
         <section className="window__main">
           <header className="window__header">

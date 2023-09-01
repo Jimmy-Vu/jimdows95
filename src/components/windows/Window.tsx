3c58efc04f73b8559fce062bd6063a570cca1d8d
Jimmy Vu <JMV3211@gmail.com>
2023-08-31 21:19:03 -0700

    Set up draggable bottom right corner of window

--- src/components/windows/Window.tsx
+++ src/components/windows/Window.tsx
@@ -5,22 +5,36 @@ export default function Window() {
   return (
     <Draggable
       handle=".window__header"
+      // bounds="parent"
+      defaultPosition={{ x: 0, y: 0 }}
       positionOffset={{ x: '-50%', y: '-50%' }}
     >
-      {/* <Resizable
+      <Resizable
+        className="window"
+        handleComponent={{ bottomRight: <img style={{ transform: 'translateX(-10px) translateY(-10px)' }} src="src/assets/resize-right.svg" alt="" /> }}
+        handleClasses={{
+          top: "pointer-events-none",
+          right: "pointer-events-none",
+          bottom: "pointer-events-none",
+          left: "pointer-events-none",
+          topRight: "pointer-events-none",
+          bottomLeft: "pointer-events-none",
+          topLeft: "pointer-events-none",
+        }}
         defaultSize={{
-          width: 620,
+          width: 600,
           height: 400
         }}
+        minWidth={600}
+        minHeight={400}
         bounds="parent"
-        style={{position: "absolute"}}
-      > */}
-      <section className="window">
-        <header className="window__header">
-
-        </header>
-      </section>
-      {/* </Resizable> */}
-    </Draggable>
+        style={{ position: 'absolute', left: '50%', top: '50%' }}
+      >
+        <section className="window__main">
+          <header className="window__header">
+          </header>
+        </section>
+      </Resizable>
+    </Draggable >
   );
 }

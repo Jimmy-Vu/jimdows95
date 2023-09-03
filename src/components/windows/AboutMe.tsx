d7d8c74c00c5ed36e3a037236bc57fe3018583cd
Jimmy Vu <JMV3211@gmail.com>
2023-09-02 20:31:12 -0700

    Change wording in the About Me paragraph

--- src/components/windows/AboutMe.tsx
+++ src/components/windows/AboutMe.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
       <h1 className="about-me__heading">About Me</h1>
       <section className="about-me__text">
         <p>Hi there! My name is Jimmy.</p>
-        <p>I've been a naturally curious tinkerer since the days of my youthful splendor.</p>
+        <p>I've been a naturally curious tinkerer since my days of youthful splendor.</p>
         <p>{`Whenever I'm not writing or consuming code, you can catch me reading Brandon Sanderson novels, lifting weights, or tinkering with electronics.`}</p>
       </section>
     </div>;

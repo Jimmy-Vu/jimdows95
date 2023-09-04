5aaca303e81fe97ed6f608856c9a0f2c6c618e6f
Jimmy Vu <JMV3211@gmail.com>
2023-09-04 16:20:24 -0700

    Add more text content to About Me and change overflow of the container to scroll instead of hidden

--- src/components/windows/AboutMe.tsx
+++ src/components/windows/AboutMe.tsx
@@ -5,11 +5,21 @@ export default function AboutMe() {
     <div className="about-me">
       <h1 className="about-me__heading">About Me</h1>
       <section className="about-me__text">
-        <p>Hi there! My name is Jimmy.</p>
-        <p>I've been a naturally curious tinkerer since my days of youthful splendor. From being the guy who everyone came to with technical issues.
-          &#40;Someone once asked me to hack a FaceBook account. Needless to say, I definitely didn't.&#41; </p>
-        <p>My journey in tech began when I was an IT Specialist at a cloud-based PaaS company.</p>
-        <p>Whenever I'm not writing or consuming code, you can catch me reading Brandon Sanderson novels, lifting weights, or messing around with electronics.</p>
+        <p>Hi there! My name is Jimmy, your next software engineer.</p>
+        <p>
+          I've been a naturally curious tinkerer and problem solver since my days of youthful splendor. Yes, I was that guy who everyone came to with technical issues.
+          &#40;Someone once asked me to hack a FaceBook account. Needless to say, I definitely didn't.&#41;
+        </p>
+        <p>
+          My journey in tech began when I was an IT Specialist at a cloud-based PaaS company, deploying and maintaining servers at the on-site data center.
+          It was there that I got to rub elbows with bright minded software engineers and gained inspiration to deepen my understanding of software. After heavy deliberation,
+          I enrolled in an intensive 14-week software engineering program, laying a solid foundation in full stack web development.
+        </p>
+        <p>
+          Since graduating, I have continued a relentless pursuit to expand my skills and intimate understanding of software by constructing mobile-responsive web applications,
+          reading technical books, and staying up to date with modern web development with podcasts and newsletters.
+        </p>
+        <p>Whenever I'm not writing or consuming code, you can catch me reading Brandon Sanderson novels, writing fiction, lifting weights, or messing around with electronics.</p>
       </section>
     </div>;
 

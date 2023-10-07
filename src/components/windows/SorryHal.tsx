83554252fbb68975abd82b8b8b6ea8b0ae20dd8d
Jimmy Vu <JMV3211@gmail.com>
2023-10-06 20:42:08 -0700

    Finish first draft of the error message

--- src/components/windows/SorryHal.tsx
+++ src/components/windows/SorryHal.tsx
@@ -1,28 +1,39 @@
 import ErrorFrame from "./ErrorFrame";
-import { useAppSelector } from "../../app/hooks";
-import { setSorryHalOpenState, setSorryHalMaxState, selectSorryHalOpenState, selectSorryHalMaxState } from "../../app/appSlice";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { setSorryHalOpenState, setSorryHalMaxState, selectSorryHalOpenState } from "../../app/appSlice";
 import ZIndexCheck from "./lib/ZIndexCheck";
 
 export default function SorryHal() {
+  const dispatch = useAppDispatch();
   const sorryHalOpenState = useAppSelector(selectSorryHalOpenState);
-  const sorryHalMaxState = useAppSelector(selectSorryHalMaxState);
   const zIdx = ZIndexCheck('sorryHal');
 
   const content =
     <div className="sorry-hal">
-      <img src="src/components/windows/assets/hal9000.gif" alt="" />
-      <p>I'm sorry Dave, I'm afraid I can't do that.</p>
+      <section className="sorry-hal__main">
+        <a href="https://youtu.be/ARJ8cAGm6JE?t=59" target="_blank">
+          <img className="sorry-hal__main__image" src="src/components/windows/assets/hal9000.gif" alt="" />
+        </a>
+        <p className="sorry-hal__main__message">I'm sorry
+          <a className="sorry-hal__main__message__link" href="https://youtu.be/ARJ8cAGm6JE?t=59" target="_blank">
+            &#32;Dave
+          </a>
+          , I'm afraid I can't do that.</p>
+      </section>
+      <section className="sorry-hal__btn-container">
+        <button onClick={() => dispatch(setSorryHalOpenState(false))} className="sorry-hal__btn-container__confirm">Okay</button>
+      </section>
     </div>;
 
   return (
     <ErrorFrame
-      title="Error!"
+      title="Error Exiting System"
       id="sorryHal"
       zIdx={zIdx}
       icon=""
       content={content}
-      defaultSize={{ width: 600, height: 400 }}
-      state={{ openState: sorryHalOpenState}}
+      defaultSize={{ width: 400, height: 150 }}
+      state={{ openState: sorryHalOpenState }}
       setOpenFunc={setSorryHalOpenState}
       setMaxFunc={setSorryHalMaxState}
     />

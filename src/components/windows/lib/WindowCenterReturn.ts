55e2906020ac7edeeead56f9cb295781f1aa80e9
Jimmy Vu <JMV3211@gmail.com>
2023-10-12 16:30:08 -0700

    Add a helper function to calculate center of screen and have the windows utilize it

---
+++ src/components/windows/lib/WindowCenterReturn.ts
@@ -0,0 +1,5 @@
+export default function WindowCenterReturn() {
+  const widthCenter = window.innerWidth / 2;
+  const heightCenter = window.innerHeight / 2;
+  return { widthCenter, heightCenter };
+}

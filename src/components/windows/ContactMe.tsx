1c395f5da755e849f8d2f6a6c2c9c540f2f72c37
Jimmy Vu <JMV3211@gmail.com>
2023-10-18 00:18:11 -0700

    Set up first iteration of the contact me form

--- src/components/windows/ContactMe.tsx
+++ src/components/windows/ContactMe.tsx
@@ -10,7 +10,28 @@ export default function ContactMe() {
 
   const content =
     <div className="contact-me">
-
+      <form action="https://api.web3forms.com/submit" method="POST">
+        <input type="hidden" name="access_key" value="4e25eb62-5459-4ae5-9b6a-2bd82e98aea8" />
+          <input type="hidden" name="subject" value="New Message from jimmyvu.co" />
+            <div className="fields">
+            <div className="field half">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" required />
+              </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" required />
+              </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" rows={5} required></textarea>
+              </div>
+            </div>
+            <input type="hidden" name="redirect" value="https://web3forms.com/success" />
+            <ul className="actions">
+              <li><a href="" className="button submit">Send Message</a></li>
+              </ul>
+      </form>
     </div>;
 
   return (

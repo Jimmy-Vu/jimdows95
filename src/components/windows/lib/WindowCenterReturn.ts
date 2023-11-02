7941e71ce1294565a23d169bf9f9ea7c08415510
Jimmy Vu <JMV3211@gmail.com>
2023-11-01 21:01:29 -0700

    Lower the taskbar height from 40px to 35px

--- src/components/windows/lib/WindowCenterReturn.ts
+++ src/components/windows/lib/WindowCenterReturn.ts
@@ -1,5 +1,5 @@
 export default function WindowCenterReturn() {
   const widthCenter = window.innerWidth / 3.5;
-  const heightCenter = (window.innerHeight - 40) / 6; // -40 to account for the height of the taskbar
+  const heightCenter = (window.innerHeight - 35) / 6; // -35 to account for the height of the taskbar
   return { widthCenter, heightCenter };
 }

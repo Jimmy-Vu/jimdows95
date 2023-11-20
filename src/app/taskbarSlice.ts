b6234578dcd16a41ea5c9a6bae0c5aa19c645fb4
Jimmy Vu <JMV3211@gmail.com>
2023-11-20 14:44:44 -0800

    Create a factory function to handle firing of Redux action creators instead of storing the creators in the state

--- src/app/taskbarSlice.ts
+++ src/app/taskbarSlice.ts
@@ -1,26 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { ActionCreatorWithPayload, PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from './store'
-import { setAboutMeOpenState } from './appSlice'
+import { setAboutMeOpenState, setContactMeOpenState, setProjectsOpenState, setSorryHalOpenState } from './appSlice';
+import { setColosseumOpenState, setJimdows95OpenState, setNHHOpenState, setVicarusOpenState } from './projectsSlice';
 
 interface AppState {
-  windows: { title: string, id: string, openFunc: ActionCreatorWithPayload<boolean> }[]
+  windows:
+  {
+    title: string,
+    id: string
+  }[]
 }
 
 const initialState: AppState = {
-  windows: [{ title: 'About Me', id: 'aboutMe', openFunc: setAboutMeOpenState }]
+  windows: [
+    {
+      title: 'About Me',
+      id: 'aboutMe'
+    }
+  ]
 }
 
 export const taskbarSlice = createSlice({
   name: 'taskbar',
   initialState,
   reducers: {
-    addWindow: (state, action: PayloadAction<{ title: string, id: string, openFunc: ActionCreatorWithPayload<boolean> }>) => {
+    addWindow: (state, action: PayloadAction<{ title: string, id: string }>) => {
       if (state.windows.every(window => JSON.stringify(window) !== JSON.stringify(action.payload))) {
         state.windows.push(action.payload);
       }
     },
-    removeWindow: (state, action: PayloadAction<{ title: string, id: string, openFunc: ActionCreatorWithPayload<boolean> }>) => {
+    removeWindow: (state, action: PayloadAction<{ title: string, id: string }>) => {
       const index = state.windows.findIndex(window => JSON.stringify(window) === JSON.stringify(action.payload));
       console.log(index)
       if (index !== -1) {
@@ -40,3 +50,18 @@ export const {
 export const selectWindows = (state: RootState) => state.taskbar.windows;
 
 export default taskbarSlice.reducer;
+
+export function createOpenWindowActionCreator(windowId: string) {
+  const actionCreatorsMap: Record<string, ActionCreatorWithPayload<boolean>> = {
+    'aboutMe': setAboutMeOpenState,
+    'projects': setProjectsOpenState,
+    'contactMe': setContactMeOpenState,
+    'sorryHal': setSorryHalOpenState,
+    'colosseum': setColosseumOpenState,
+    'vicarus': setVicarusOpenState,
+    'jimdows95': setJimdows95OpenState,
+    'nhh': setNHHOpenState,
+  }
+
+  return actionCreatorsMap[windowId];
+}
